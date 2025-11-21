4af6ee48aa08671397d5860d2aad526e722457f6
Faiznurullah <faizn103a@gmail.com>
2025-11-21 14:06:29 +0700

    Transaction page Temp

---
+++ lib/presentation/transaction_add_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TransactionAddPage extends StatefulWidget {
+  const TransactionAddPage({super.key});
+
+  @override
+  State<TransactionAddPage> createState() => _TransactionAddPageState();
+}
+
+class _TransactionAddPageState extends State<TransactionAddPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold( 
+       
+    );
+  }
+}
---
+++ lib/presentation/transaction_edit_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TransactionEditPage extends StatefulWidget {
+  const TransactionEditPage({super.key});
+
+  @override
+  State<TransactionEditPage> createState() => _TransactionEditPageState();
+}
+
+class _TransactionEditPageState extends State<TransactionEditPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold( 
+       
+    );
+  }
+}
